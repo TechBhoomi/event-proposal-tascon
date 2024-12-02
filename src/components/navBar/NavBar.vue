5a5f3135711479f9a4f59c205c8c0c9954dd9e3a
Aliston Gomes <112464730+aliston-gomes@users.noreply.github.com>
2024-12-02 19:41:41 +0530

    navbar alignment fixed

--- src/components/navBar/NavBar.vue
+++ src/components/navBar/NavBar.vue
@@ -1,27 +1,28 @@
 <template>
-  <v-app>
-    <v-container>
-      <v-app-bar :elevation="2">
-        <!-- <template v-slot:prepend>
+  <!-- <v-container> -->
+  <v-app-bar :elevation="2">
+    <!-- <template v-slot:prepend>
           <v-app-bar-nav-icon></v-app-bar-nav-icon>
         </template> -->
 
-        <v-app-bar-title
-          class="font-bold text-2xl text-gray-600"
-          style="font-weight: 600;"
-          >Application Bar</v-app-bar-title
-        >
+    <v-app-bar-title
+      class="font-bold text-2xl text-gray-600"
+      style="font-weight: 600"
+      >Application Bar</v-app-bar-title
+    >
 
-        <!-- <template v-slot:append>
+    <!-- <template v-slot:append>
           <v-btn icon="mdi-heart"></v-btn>
 
           <v-btn icon="mdi-magnify"></v-btn>
 
           <v-btn icon="mdi-dots-vertical"></v-btn>
         </template> -->
-      </v-app-bar>
-    </v-container>
-  </v-app>
+  </v-app-bar>
+  <!-- </v-container> -->
+  <!-- <Carousel /> -->
 </template>
-<script setup></script>
+<script setup>
+import Carousel from "../carousel/Carousel.vue";
+</script>
 <style scoped></style>
