b38d30fd27e5c0aa1087ddc352a1c60a7a609e9c
Aliston Gomes <112464730+aliston-gomes@users.noreply.github.com>
2024-12-02 18:11:06 +0530

    carousel and navbar mounted

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <script setup>
 import Carousel from "./components/carousel/Carousel.vue";
-
 import NavBar from "./components/navBar/NavBar.vue";
 </script>
 
 <template>
-  <section>
-    <NavBar />
-    <Carousel />
-  </section>
+  <!-- <section> -->
+  <NavBar />
+  <Carousel />
+  <!-- </section> -->
 </template>
 
 <style scoped></style>
