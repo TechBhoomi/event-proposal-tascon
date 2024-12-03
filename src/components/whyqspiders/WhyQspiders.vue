05220a2a2c4a3727b54ebcac4f7b719a17c823d8
Manojkumar <manojkumarme022@gmail.com>
2024-12-03 18:22:03 +0530

    Why qspiders component created

---
+++ src/components/whyqspiders/WhyQspiders.vue
@@ -0,0 +1,57 @@
+<template>
+  <section class="p-2 lg:w-[100%] text-justify">
+    <h1 class="text-h3 font-weight-bold pb-1">Why Qspiders?</h1>
+    <section class="text-xl">
+      <ul class="list-disc pl-6">
+        <li>
+          Qspiders provides industry-relevant training to bridge the gap between
+          academics and the IT industry.
+        </li>
+        <li>
+          Experienced mentors ensure hands-on learning, preparing students for
+          real-world challenges.
+        </li>
+        <li>
+          Extensive placement support helps students secure jobs in top
+          companies globally.
+        </li>
+        <li>
+          Specialized courses in software testing and development to enhance
+          technical skills.
+        </li>
+        <li>
+          Interactive sessions and regular assessments boost confidence and
+          learning outcomes.
+        </li>
+        <li>
+          Qspiders provides industry-relevant training to bridge the gap between
+          academics and the IT industry.
+        </li>
+        <li>
+          Experienced mentors ensure hands-on learning, preparing students for
+          real-world challenges.
+        </li>
+        <li>
+          Extensive placement support helps students secure jobs in top
+          companies globally.
+        </li>
+        <li>
+          Specialized courses in software testing and development to enhance
+          technical skills.
+        </li>
+        <li>
+          Interactive sessions and regular assessments boost confidence and
+          learning outcomes.
+        </li>
+      </ul>
+    </section>
+  </section>
+</template>
+
+<script setup></script>
+
+<style scoped>
+li {
+  padding: 10px;
+}
+</style>
