0e7d7b9f922925f7e73cb5f0115d177c09a53b4d
Aliston Gomes <112464730+aliston-gomes@users.noreply.github.com>
2024-12-03 16:10:00 +0530

    course data static removed

--- src/components/cards/Cards.vue
+++ src/components/cards/Cards.vue
@@ -1,88 +1,95 @@
-<script>
-export default {
-  data() {
-    return {
-      cards: [
-        {
-          para1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-          para2: "Suspendisse varius enim in eros elementum tristique.",
-          buttonText: "Learn More",
-        },
-        {
-          para1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-          para2: "Suspendisse varius enim in eros elementum tristique.",
-          buttonText: "Learn More",
-        },
-        {
-          para1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-          para2: "Suspendisse varius enim in eros elementum tristique.",
-          buttonText: "Learn More",
-        },
-        {
-          para1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-          para2: "Suspendisse varius enim in eros elementum tristique.",
-          buttonText: "Learn More",
-        },
-        {
-          para1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-          para2: "Suspendisse varius enim in eros elementum tristique.",
-          buttonText: "Learn More",
-        },
-        {
-          para1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-          para2: "Suspendisse varius enim in eros elementum tristique.",
-          buttonText: "Learn More",
-        },
-      ],
-    };
-  },
-};
+<script setup>
+import { onBeforeMount } from "vue";
+import { globalStore } from "../../store/globalStore";
+
+// export default {
+//   data() {
+//     return {
+//       cards: [
+//         {
+//           para1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+//           para2: "Suspendisse varius enim in eros elementum tristique.",
+//           buttonText: "Learn More",
+//         },
+//         {
+//           para1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+//           para2: "Suspendisse varius enim in eros elementum tristique.",
+//           buttonText: "Learn More",
+//         },
+//         {
+//           para1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+//           para2: "Suspendisse varius enim in eros elementum tristique.",
+//           buttonText: "Learn More",
+//         },
+//         {
+//           para1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+//           para2: "Suspendisse varius enim in eros elementum tristique.",
+//           buttonText: "Learn More",
+//         },
+//         {
+//           para1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+//           para2: "Suspendisse varius enim in eros elementum tristique.",
+//           buttonText: "Learn More",
+//         },
+//         {
+//           para1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+//           para2: "Suspendisse varius enim in eros elementum tristique.",
+//           buttonText: "Learn More",
+//         },
+//       ],
+//     };
+//   },
+// };
+const GLOBAL_STORE = globalStore();
+onBeforeMount(async () => {
+  GLOBAL_STORE.getCourseList();
+});
 </script>
 
 <template>
-      <section class="p-2">
+  <section class="p-2">
     <h1 class="text-h3 font-weight-medium pb-1">Courses</h1>
-  <v-row dense>
-    <v-col
-      cols="12"
-      sm="4"
-      md="3"
-      lg="2"
-      gap="2"
-      align="center"
-      v-for="(card, index) in cards"
-      :key="index"
-    >
-      <v-card
-        class="rounded-lg p-2"
-        elevation="2"
-        max-height="195"
-        color="#efefef"
+    <v-row dense>
+      <v-col
+        cols="12"
+        sm="4"
+        md="3"
+        lg="4"
+        gap="2"
+        align="center"
+        v-for="(card, index) in cards"
+        :key="index"
       >
-        <v-card-text>
-          <p>
-            {{ card.para1 }}
-          </p>
-          <p>
-            {{ card.para2 }}
-          </p>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn
-            color="primary"
-            block
-            slim="true"
-            rounded
-            size="small"
-            variant="tonal"
-          >
-            {{ card.buttonText }}
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
-</section>
+        <v-card
+          class="rounded-lg p-2"
+          elevation="2"
+          max-height="195"
+          color="#efefef"
+        >
+          <v-card-text>
+            <p>
+              {{ card.para1 }}
+            </p>
+            <p>
+              {{ card.para2 }}
+            </p>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn
+              color="primary"
+              block
+              slim="true"
+              rounded
+              size="small"
+              variant="tonal"
+            >
+              {{ card.buttonText }}
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </section>
 </template>
 
 <style scoped>
