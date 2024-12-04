96d821cd00b9e889bfdb989b3e82433db52124f6
Manojkumar <manojkumarme022@gmail.com>
2024-12-04 18:01:16 +0530

    Background image changed in about page

--- src/components/about/About.vue
+++ src/components/about/About.vue
@@ -8,10 +8,25 @@
         About Qspiders
       </h1>
       <section class="text-xl font-poppins text-justify">
-        QSpiders - the leading technology training organization, was started
+        <!-- QSpiders - the leading technology training organization, was started
         with the goal of bridging the gap between industry demands and academic
         curriculums. It is designed to work as a 'Finishing School' and is
-        modelled with several learning programs.
+        modelled with several learning programs. -->
+        <p>
+          QSpiders is the world’s ace software training organization with an aim
+          to bridge the gap between the demands of the industry and the
+          curriculum of educational institutions.
+        </p>
+        <br />
+        <p>
+          With centers across the Globe, the institute is a platform where young
+          minds are given the opportunity to build successful careers.
+        </p>
+        <br />
+        <p>
+          ”QSpiders is a place where businesses find talent and dreams take
+          flight."
+        </p>
       </section>
     </article>
   </section>
@@ -35,7 +50,9 @@ const headingClasses = computed(() => {
 <style scoped>
 #about-section {
   padding: 1rem;
-  background: linear-gradient(rgba(0, 0, 0, 0.048), rgba(0, 0, 0, 0.055)),
-    url("../../assets/about_bg_image.jpg") center/cover no-repeat;
+  /* background: linear-gradient(rgba(0, 0, 0, 0.048), rgba(0, 0, 0, 0.055)),
+    url("https://devplaced.qspiders.com/illustrate_wave.svg") center/cover no-repeat; */
+    background: linear-gradient(rgba(0, 0, 0, 0.048), rgba(0, 0, 0, 0.055)),
+    url("https://devplaced.qspiders.com/_next/image?url=%2FBackgroundWave.png&w=384&q=75") center/cover no-repeat;
 }
 </style>
