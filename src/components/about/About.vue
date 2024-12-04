1e8ff4d175946a8cd3f3fa9fb9c5f744d17daf77
Manojkumar <manojkumarme022@gmail.com>
2024-12-04 13:26:29 +0530

    About content name changed in about page

--- src/components/about/About.vue
+++ src/components/about/About.vue
@@ -1,13 +1,11 @@
 <template>
-  <section
-    class="lg:pt-10 lg:pb-10 pt-7 pb-7 lg:w-[50%] text-justify "
-  >
+  <section class="lg:pt-10 lg:pb-10 pt-7 pb-7 lg:w-[60%] text-justify">
     <h1 class="text-h3 font-weight-bold pb-4">About Qspiders</h1>
     <section class="text-xl">
-      World's premier software training institution, dedicated to bridging the
-      gap between industry demands and academic curricula. With centers
-      worldwide, Qspiders provides a platform for young minds to cultivate
-      successful careers
+      QSpiders - the leading technology training organization, was started with
+      the goal of bridging the gap between industry demands and academic
+      curriculums. It is designed to work as a 'Finishing School' and is
+      modelled with several learning programs.
     </section>
   </section>
 </template>
