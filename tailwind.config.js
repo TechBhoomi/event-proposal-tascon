c009a46da8a858c9d78bdb5cc880acdda092ca87
Aliston Gomes <112464730+aliston-gomes@users.noreply.github.com>
2024-12-04 14:12:28 +0530

    fonts added popins and sans

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['"Poppins"', "sans-serif"], // Custom Poppins font
+        sans: ["ui-sans-serif", "system-ui"], // Tailwind's default sans-serif font/ Add 'Open Sans' to the sans family
+      },
+    },
   },
   plugins: [],
-}
+};
